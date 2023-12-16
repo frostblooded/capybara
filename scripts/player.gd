4ccf75e4e143c9babc5937a40dd1bf2434ac8dca
Nikolay Danailov <frostblooded@yahoo.com>
2023-12-16 10:26:01 +0200

    Add basic player movement

---
+++ scripts/player.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+@export var speed: float = 400
+
+func get_input() -> void:
+    var input_direction: Vector2 = Input.get_vector("left", "right", "up", "down")
+    velocity = input_direction * speed
+
+
+func _physics_process(_delta: float) -> void:
+    get_input()
+    move_and_slide()
